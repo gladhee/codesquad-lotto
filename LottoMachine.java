e0caa7a51d29a3514b2ef752eadadf412a5e6cf8
jangsubin <eltm521@naver.com>
2024-12-17 15:14:31 +0900

    feat : 공끼리 부딪힌 경우 검증 메소드 작성

--- LottoMachine.java
+++ LottoMachine.java
@@ -1,7 +1,12 @@
 public class LottoMachine {
 
-    static final int MAX_WALL = 20;
-    static final int MIN_WALL = -1;
+    private static final int MAX_WALL = 20;
+    private static final int MIN_WALL = -1;
+    // 각 좌표마다의 공 갯수
+    int[][] ballCount = new int[MAX_WALL][MAX_WALL];
+    //각 좌표위에 올려진 공 번호
+    int[][] machine = new int[MAX_WALL][MAX_WALL];
+
 
     public void checkWin(LottoBall lottoBall) {
         if (lottoBall.x == 0 && lottoBall.y == 0) {
@@ -12,6 +17,15 @@ public class LottoMachine {
     public boolean hitWall(LottoBall lottoBall) {
         if (lottoBall.x == MAX_WALL || lottoBall.x == MAX_WALL || lottoBall.y == MAX_WALL || lottoBall.y == MAX_WALL){
             return false;
+        }
+        return true;
+    }
+
+    public boolean hitBall() {
+        for (int i = 0; i < MAX_WALL; i++) {
+            for (int j = 0; j < MAX_WALL; j++) {
+                if (ballCount[i][j] > 1) return false;
+            }
         }
         return true;
     }
