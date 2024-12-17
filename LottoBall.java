248d2ef463b1d220e4713eba2cb6c4096b9d3c80
gladhee <gladhee@icloud.com>
2024-12-17 15:12:07 +0900

    feat(LottoBall): 로또 공 클래스 작성
    - move: 공의 위치 업데이트
    
    - setSelected: 당첨 공 설정

---
+++ LottoBall.java
@@ -0,0 +1,24 @@
+public class LottoBall {
+
+    private final int number;
+    private int x;
+    private int y;
+    private boolean isSelected;
+
+    public LottoBall(int number, int x, int y) {
+        this.number = number;
+        this.x = x;
+        this.y = y;
+        this.isSelected = false;
+    }
+
+    public void move(int x, int y) {
+        this.x += x;
+        this.y += y;
+    }
+
+    public void setSelected() {
+        this.isSelected = true;
+    }
+
+}
