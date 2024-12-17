6051d0b5548f2659754c5b2fae242daa9a74cc83
gladhee <gladhee@icloud.com>
2024-12-17 15:07:59 +0900

    feat(NumberGenerator): 랜덤 숫자 생성기 클래스 작성
    - generateLottoNumber: 1 ~ 45 사이 겹치지 않는 로또 번호 생성
    
    - generateMovement: -1 ~ 1 사이 숫자의 랜덤 이동 생성
    
    - generateInitIndex: 공의 첫 시작 좌표 생성

---
+++ NumberGenerator.java
@@ -0,0 +1,38 @@
+import java.util.HashSet;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class NumberGenerator {
+
+    private final Random random;
+    private final HashSet<Integer> alreadyNumbers;
+
+    public NumberGenerator() {
+        this.random = ThreadLocalRandom.current();
+        this.alreadyNumbers = new HashSet<>();
+    }
+
+    public int generateLottoNumber() {
+        int number = pickRandomNumber(1, 45);
+
+        while (!alreadyNumbers.contains(number)) {
+            number = pickRandomNumber(1, 45);
+        }
+        alreadyNumbers.add(number);
+
+        return number;
+    }
+
+    public int generateMovement() {
+        return pickRandomNumber(-1, 1);
+    }
+
+    public int generateInitIndex() {
+        return pickRandomNumber(0, 20);
+    }
+
+    private int pickRandomNumber(int minValue, int maxValue) {
+        return random.nextInt(maxValue - minValue + 1) + minValue;
+    }
+
+}
